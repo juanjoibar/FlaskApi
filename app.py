ca7671da1eb869482d97f79c97aa9049db5ef9e9
juanjoibar <juanjoibar@gmail.com>
2021-12-19 22:42:06 -0300

    Configuracion de base y consulta a empleados.

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-import flask import Flask, jsons
+from flask import Flask
+from flask import render_template,request, url_for
+from pymysql import cursors
+from werkzeug.utils import redirect
 from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_BD']='sistemas1121' 
+mysql.init_app(app)
+
 
 @app.route('/')
 
@@ -7,5 +19,18 @@ from flaskext.mysql import MySQL
 @app.route('/empleados')
 def empleados()
     #no se porque el delete
-    sql = 'select * from empleados where is_delete = 0'
-    cursor
+    sql = 'select * from `sistemas1121`.`empleados` ;'
+    conn = mysql.connect()
+
+    cursor = conn.cursor()
+    
+    cursor.execute(sql)
+    empleados = cursor.fetchall()
+
+    conn.commit()
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
