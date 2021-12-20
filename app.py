d5c3de4e8a23717c2376de54b00124a94a68508a
juanjoibar <juanjoibar@gmail.com>
2021-12-20 15:33:59 -0300

    creando index

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import render_template,request, url_for
 from pymysql import cursors
-from werkzeug.utils import redirect
+from werkzeug.utils import append_slash_redirect, redirect
 from flaskext.mysql import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
@@ -14,10 +16,26 @@ mysql.init_app(app)
 
 
 @app.route('/')
-
+def idex():
+    return '<h2>Esto es una API</h2> \
+    <p><a href="/empleados">/empleados</a></p>'
 
 @app.route('/empleados')
-def empleados()
+def empleados():
+    #no se porque el delete
+    sql = 'select * from `sistemas1121`.`empleados` ;'
+    conn = mysql.connect()
+
+    cursor = conn.cursor()
+    
+    cursor.execute(sql)
+    empleados = cursor.fetchall()
+
+    conn.commit()
+
+
+@app.route('/empleado')
+def empleado():
     #no se porque el delete
     sql = 'select * from `sistemas1121`.`empleados` ;'
     conn = mysql.connect()
